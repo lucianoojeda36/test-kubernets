6305593d4416ddd3853b33cb7a191d1d2e17c07a
Luciano Ojeda <lucianoojeda36@gmail.com>
2025-10-24 10:36:37 -0300

    Agregar archivos del frontend (resolver problema de submódulo)

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [mensaje, setMensaje] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('http://localhost:30001/api/mensaje')
+      .then(res => res.json())
+      .then(data => {
+        setMensaje(data.mensaje);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.error(err);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mi App en Kubernetes prueba cambio 2 ddd rrr</h1>
+        {loading ? <p>Cargando...</p> : <p>{mensaje}</p>}
+      </header>
+    </div>
+  );
+}
+
+export default App;
